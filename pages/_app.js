7a2810394fbf9e5b553f436e8545c74f0a0c9d39
boom <nuntipatcpe@gmail.com>
2022-08-13 22:30:04 +0700

    feat: Add Head <title>

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,15 @@ import "../styles/reset.css";
 import Navbar from "../layout/Navbar";
 import Aside from "../layout/Aside";
 
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Documentation</title>
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1"/> */}
+      </Head>
       <Navbar />
       <div className="flex justify-center mt-10 bg-neutral-900">
         {/* <div className="w-full h-20 bg-neutral-600"></div> */}
