00c6a261ae5bb02d0599ce3ce450fa0faeb54498
boom <nuntipatcpe@gmail.com>
2022-08-15 23:48:47 +0700

    deploy 404

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ function PageNotFound(){
 
 
     return (
-        <div className="max-w-[720px] w-[720px] p-10 bg-neutral-50 overflow-auto flex items-center justify-center">
+        <div className="max-w-[720px] w-[720px] p-10 bg-neutral-50 overflow-auto flex items-center justify-center h-[100vh]">
               <p className="font-bold">404 | This page could not be found.</p> 
         </div>
     )
